fede6621681dfca1964514625774443bc0203251
TrueBurn <ricus.nortje@gmail.com>
2025-01-14 21:10:54 +0200

    Add browser launch functionality to giveaway overlay

--- FirebotGiveawayObsOverlay/FirebotGiveawayObsOverlay.WebApp/Program.cs
+++ FirebotGiveawayObsOverlay/FirebotGiveawayObsOverlay.WebApp/Program.cs
@@ -1,5 +1,7 @@
 using FirebotGiveawayObsOverlay.WebApp.Components;
 using FirebotGiveawayObsOverlay.WebApp.Helpers;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
@@ -24,6 +26,28 @@ app.Lifetime.ApplicationStarted.Register(() =>
     GiveAwayHelpers.SetFireBotFileFolder(fileBotFileFolder);
     int giveawayDuration = app.Configuration.GetValue<int>("AppSettings:GiveawayDuration", 69);
     GiveAwayHelpers.SetGiveawayDuration(giveawayDuration);
+
+    // Launch browser
+    string url = "http://localhost:5243/giveaway";
+    try
+    {
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+        {
+            Process.Start(new ProcessStartInfo(url) { UseShellExecute = true });
+        }
+        else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+        {
+            Process.Start("xdg-open", url);
+        }
+        else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+        {
+            Process.Start("open", url);
+        }
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Failed to open browser: {ex.Message}");
+    }
 });
 
 //app.UseHttpsRedirection();
